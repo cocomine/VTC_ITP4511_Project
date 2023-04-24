d55194c5cece760dc65c31929943392bee537528
cocomine <roguish.6888467468@gmail.com>
2023-04-24 23:05:49 +0800

    link update

---
+++ src/main/java/it/itp4511/ea/servlet/venue/createVenue.java
@@ -0,0 +1,22 @@
+package it.itp4511.ea.servlet.venue;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "createVenue", value = {"/venue/create", "/venue/create/"})
+public class createVenue extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/it/itp4511/ea/servlet/venue/viewVenue.java
@@ -0,0 +1,22 @@
+package it.itp4511.ea.servlet.venue;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "viewVenue", value = {"/venue", "/venue/"})
+public class viewVenue extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
